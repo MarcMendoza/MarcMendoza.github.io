241f4c8a846e746ade107fc97c38288930dbb820
Marc Mendoza <synth477@gmail.com>
2022-01-22 01:34:33 -0600

    Setup whose turn page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let startDate = new Date(2021, 1, 1);
+  let oneDay = 1000 * 60 * 60 * 24;
+  let today = new Date();
+
+  let numOfDaysDif = (today.getTime() - startDate.getTime()) / oneDay;
+  let isEven = numOfDaysDif % 2
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Whose turn is it today?
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {isEven === 0 ? "Blackhole" : "Demon Princess"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
